2cdc1be242a41479da858c6cfb9e3fad734bf12b
alejo <alejo200003@gmail.com>
2023-03-20 20:23:05 -0300

    configuración de servidor local

--- src/app.js
+++ src/app.js
@@ -1,1 +1,5 @@
-console.log ("hola mundo");
+const express = require('express');
+const app = express();
+const Port = 3001;
+app.get('/api',(req , res) =>{console.log ('PASO POR ACA'), res.json({mensaje:'¡HOLA MUNDO!'}).status(200)});
+app.listen(Port,()=>{console.log('ESTOY ESCUCHANDO EN EL PUERTO', Port)});
